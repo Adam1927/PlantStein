2c061ede2ce67e57434dd05b4bca98fd912d19f4
serbina@net.chalmers.se <serbina@net.chalmers.se>
2023-04-29 16:54:51 +0300

    Set up mqtt #14

---
+++ server/src/main/java/com/plantstein/server/mqtt/MQTTBeans.java
@@ -0,0 +1,75 @@
+package com.plantstein.server.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+@Configuration
+public class MQTTBeans {
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        String[] uri = new String[]{"tcp://localhost:1883"};
+        options.setServerURIs(uri);
+        options.setCleanSession(true);
+
+        factory.setConnectionOptions(options);
+        return factory;
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducer inBound() {
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("serverIn", mqttClientFactory(), "#");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(2);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return message -> {
+            String topic = message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC).toString();
+            if (topic.equals("myTopic")) {
+                System.out.println("This is our topic");
+            }
+            System.out.println(message.getPayload());
+        };
+    }
+
+    @Bean
+    public MessageChannel mqttOutBoundChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttOutBoundChannel")
+    public MessageHandler mqttOutBound() {
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler("serverOut", mqttClientFactory());
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic("#");
+        messageHandler.setDefaultRetained(false);
+        return messageHandler;
+    }
+
+
+}
