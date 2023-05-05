68e8f61f94175709c60fbcd2a4f44d5bba4348a5
Michael Koenig <koenigmichael2002@gmail.com>
2023-05-05 13:42:58 +0200

    Generate room details rows dynamically #19

--- frontend/lib/room_details.dart
+++ frontend/lib/room_details.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:plant_stein/utils.dart';
 import 'bottom_navigation.dart';
 import 'pot_details.dart';
 import 'package:http/http.dart' as http;
@@ -17,15 +19,16 @@ class _RoomDetailsState extends State<RoomDetails> {
   List<String> pots = [];
 
   @override
-  Widget build(BuildContext context) {
-    getPots(widget.room);
-    int potNumber = pots.length;
-    pots.addAll(List.filled(9 - potNumber, ''));
-    List<bool> visiblePots = List.filled(9, false);
+  void initState() {
+    super.initState();
+    debugPrint("initState");
+    // load pots only once (when widget is created)
+    loadPots(widget.room);
+  }
 
-    for (int i = 0; i < potNumber; i++) {
-      visiblePots[i] = true;
-    }
+  @override
+  Widget build(BuildContext context) {
+    debugPrint("build");
 
     return Scaffold(
       appBar: AppBar(
@@ -54,105 +57,7 @@ class _RoomDetailsState extends State<RoomDetails> {
                 color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 30),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              potButton(pots[0], visiblePots[0]),
-              potButton(pots[1], visiblePots[1]),
-              potButton(pots[2], visiblePots[2]),
-            ],
-          ),
-          Image.asset('images/shelf.png'),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[0], visiblePots[0]),
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[1], visiblePots[1]),
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[2], visiblePots[2]),
-              const SizedBox(
-                width: 60,
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              potButton(pots[3], visiblePots[3]),
-              potButton(pots[4], visiblePots[4]),
-              potButton(pots[5], visiblePots[5]),
-            ],
-          ),
-          Image.asset('images/shelf.png'),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[3], visiblePots[3]),
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[4], visiblePots[4]),
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[5], visiblePots[5]),
-              const SizedBox(
-                width: 60,
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              potButton(pots[6], visiblePots[6]),
-              potButton(pots[7], visiblePots[7]),
-              potButton(pots[8], visiblePots[8]),
-            ],
-          ),
-          Image.asset('images/shelf.png'),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[6], visiblePots[6]),
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[7], visiblePots[7]),
-              const SizedBox(
-                width: 60,
-              ),
-              potTitle(pots[8], visiblePots[8]),
-              const SizedBox(
-                width: 60,
-              ),
-            ],
-          ),
+          ...getUIRows()
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
@@ -164,42 +69,33 @@ class _RoomDetailsState extends State<RoomDetails> {
     );
   }
 
-  Visibility potButton(String potName, bool isVisible) {
-    return Visibility(
-        visible: isVisible,
-        maintainState: true,
-        maintainAnimation: true,
-        maintainSize: true,
-        child: IconButton(
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => PotDetails(potName)));
-          },
-          icon: Image.asset(
-            'images/pot.png',
-          ),
-          iconSize: 120,
-          padding: EdgeInsets.zero,
-        ));
+  Widget getPotButton(String potName) {
+    return IconButton(
+      onPressed: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => PotDetails(potName)));
+      },
+      icon: Image.asset(
+        'images/pot.png',
+      ),
+      iconSize: 120,
+      padding: EdgeInsets.zero,
+    );
   }
 
-  Expanded potTitle(String potName, bool isVisible) {
+  Widget getPotTitle(String potName) {
     return Expanded(
-        child: Visibility(
-      visible: isVisible,
-      maintainState: true,
-      maintainAnimation: true,
-      maintainSize: true,
       child: Text(
         potName,
         overflow: TextOverflow.ellipsis,
         textAlign: TextAlign.center,
         style: GoogleFonts.playfairDisplay(color: Colors.black),
       ),
-    ));
+    );
   }
 
-  void getPots(String room) async {
+  void loadPots(String room) async {
+    debugPrint("loadPots");
     var url = Uri.http('192.168.50.238:8080', 'room/$room/plants');
     final response = await http
         .get(url, headers: {'clientId': 'TEST_DEVICE', 'roomName': room});
@@ -207,5 +103,40 @@ class _RoomDetailsState extends State<RoomDetails> {
     setState(() {
       pots = List<String>.from(jsonData.map((item) => item["nickname"]));
     });
+  }
+
+  List<Widget> getUIRows() {
+    debugPrint("getUIRows");
+    List<Widget> result = [];
+
+    debugPrint(splitIntoBatches(pots, 3).toString());
+
+    for (List<String> threePots in splitIntoBatches(pots, 3)) {
+      List<Widget> row = [
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: threePots
+              .map<Widget>((potName) => getPotButton(potName))
+              .toList(),
+        ),
+        Image.asset('images/shelf.png'),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children:
+              threePots.map<Widget>((potName) => getPotTitle(potName)).toList(),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ];
+
+      result.addAll(row);
+
+      threePots = pots.take(3).toList();
+    }
+
+    return result;
   }
 }
